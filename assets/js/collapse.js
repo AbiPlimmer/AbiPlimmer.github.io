705f6fc45b2d44938caef3191172682e778f3bf5
AbiPlimmer <92371058+AbiPlimmer@users.noreply.github.com>
2024-09-13 15:06:41 +0100

    Update collapse.js

--- assets/js/collapse.js
+++ assets/js/collapse.js
@@ -1,9 +1,9 @@
 function collapse_pub() {
-  var coll = document.getElementsByClassName("collapsible");
+  var collp = document.getElementsByClassName("collapsible_pub");
   var i;
 
-  for (i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function() {
+  for (i = 0; i < collp.length; i++) {
+    collp[i].addEventListener("click", function() {
       this.classList.toggle("active");
       var content = this.nextElementSibling;
       if (content.style.display === "block") {
