3f05df476f42fb02d3970b1eacf2ef2f34eb8111
AbiPlimmer <92371058+AbiPlimmer@users.noreply.github.com>
2024-09-13 14:30:40 +0100

    Update collapse.js

--- assets/js/collapse.js
+++ assets/js/collapse.js
@@ -1,5 +1,4 @@
-function collapse() {
-  var coll = document.getElementsByClassName("collapsible");
+var coll = document.getElementsByClassName("collapsible");
   var i;
 
   for (i = 0; i < coll.length; i++) {
